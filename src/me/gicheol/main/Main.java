e3861a0ff1d2449cc7ba5da08cf55ba6cde6bc4f
LeeGiCheol <asdqwek200@naver.com>
2021-06-20 16:11:18 +0900

    문제가 많은 기본적인 DAO

---
+++ src/me/gicheol/main/Main.java
@@ -0,0 +1,29 @@
+package me.gicheol.main;
+
+import me.gicheol.dao.UserDao;
+import me.gicheol.domain.User;
+
+import java.sql.SQLException;
+
+public class Main {
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        UserDao userDao = new UserDao();
+
+        User user = new User();
+        user.setId("LEEGICHEOL");
+        user.setName("기철");
+        user.setPassword("asdfg");
+
+        userDao.add(user);
+
+        System.out.println(user.getId() + " 등록 성공");
+
+        User user2 = userDao.get(user.getId());
+        System.out.println("user2.getName() = " + user2.getName());
+        System.out.println("user2.getPassword() = " + user2.getPassword());
+
+        System.out.println(user2.getId() + " 조회 성공");
+    }
+
+}
