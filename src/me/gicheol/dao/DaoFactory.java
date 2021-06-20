12b4044f7886d4c00c8e9fd9ddb76b4fa183c105
LeeGiCheol <asdqwek200@naver.com>
2021-06-20 23:36:07 +0900

    다양한 Dao가 생길 것을 가정한 DaoFactory 클래스 리팩토링

--- src/me/gicheol/dao/DaoFactory.java
+++ src/me/gicheol/dao/DaoFactory.java
@@ -1,10 +1,22 @@
 package me.gicheol.dao;
 
+
 public class DaoFactory {
 
     public UserDao userDao() {
-        ConnectionMaker connectionMaker = new NConnectionMaker();
-        return new UserDao(connectionMaker);
+        return new UserDao(connectionMaker());
+    }
+
+    public AccountDao accountDao() {
+        return new AccountDao(connectionMaker());
+    }
+
+    public MessageDao messageDao() {
+        return new MessageDao(connectionMaker());
+    }
+
+    private ConnectionMaker connectionMaker() {
+        return new DConnectionMaker();
     }
 
 }
