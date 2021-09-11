0fa335d90630ebe83d772e096ae4b632c2e7af4d
LeeGiCheol <asdqwek200@naver.com>
2021-09-12 02:48:10 +0900

    자바 @Configuration -> applicationContext.xml 로 이전

--- src/me/gicheol/main/UserDaoTest.java
+++ src/me/gicheol/main/UserDaoTest.java
@@ -4,13 +4,14 @@ import me.gicheol.dao.*;
 import me.gicheol.domain.User;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.sql.SQLException;
 
 public class UserDaoTest {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactory.class);
+        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
         UserDao userDao = context.getBean("userDao", UserDao.class);
 
         User user = new User();
