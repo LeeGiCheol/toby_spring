b4c17abdb3f2505c6eeadb16880901d511854c0a
LeeGiCheol <asdqwek200@naver.com>
2021-09-18 20:31:19 +0900

    UserDao Strategy 패턴 도입

--- src/me/gicheol/dao/UserDao.java
+++ src/me/gicheol/dao/UserDao.java
@@ -63,29 +63,8 @@ public class UserDao {
 
 
     public void deleteAll() throws SQLException {
-        Connection c = null;
-        PreparedStatement ps = null;
-
-        try {
-            c = dataSource.getConnection();
-            ps = c.prepareStatement("DELETE FROM users");
-
-            ps.execute();
-        } catch (SQLException e) {
-            throw e;
-        } finally {
-            if (ps != null) {
-                try {
-                    ps.close();
-                } catch (SQLException e) {}
-            }
-
-            if (c != null) {
-                try {
-                    c.close();
-                } catch (SQLException e) {}
-            }
-        }
+        StatementStrategy st = new DeleteAllStatement();
+        jdbcContextWithStatementStrategy(st);
     }
 
     public int getCount() throws SQLException {
@@ -109,6 +88,31 @@ public class UserDao {
                     rs.close();
                 } catch (SQLException e) {}
             }
+            if (ps != null) {
+                try {
+                    ps.close();
+                } catch (SQLException e) {}
+            }
+            if (c != null) {
+                try {
+                    c.close();
+                } catch (SQLException e) {}
+            }
+        }
+    }
+
+    public void jdbcContextWithStatementStrategy(StatementStrategy stmt) throws SQLException {
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = dataSource.getConnection();
+            ps = stmt.makePreparedStatement(c);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw e;
+        } finally {
             if (ps != null) {
                 try {
                     ps.close();
