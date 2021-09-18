fa5065a30d60b6e8c3801c6ec5f592894443873c
LeeGiCheol <asdqwek200@naver.com>
2021-09-18 20:09:23 +0900

    JUnit 학습 테스트

--- src/me/gicheol/main/JUnitMain.java
+++ src/me/gicheol/main/JUnitMain.java
@@ -5,7 +5,8 @@ import org.junit.runner.JUnitCore;
 public class JUnitMain {
 
     public static void main(String[] args) {
-        JUnitCore.main("me.gicheol.main.UserDaoTest");
+//        JUnitCore.main("me.gicheol.main.UserDaoTest");
+        JUnitCore.main("me.gicheol.main.JUnitTest");
     }
 
 }
