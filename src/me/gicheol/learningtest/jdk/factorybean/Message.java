c297e8cd9d5fde47c39263aec6af87b9e0bb7d96
LeeGiCheol <asdqwek200@naver.com>
2021-09-21 02:18:06 +0900

    FactoryBean 학습

---
+++ src/me/gicheol/learningtest/jdk/factorybean/Message.java
@@ -0,0 +1,19 @@
+package me.gicheol.learningtest.jdk.factorybean;
+
+public class Message {
+
+    String text;
+
+    private Message(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public static Message newMessage(String text) {
+        return new Message(text);
+    }
+
+}
