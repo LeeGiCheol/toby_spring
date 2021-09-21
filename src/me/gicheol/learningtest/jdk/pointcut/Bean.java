830b4900e62ad257d03b542031f2b9f3a1a6c052
LeeGiCheol <asdqwek200@naver.com>
2021-09-21 19:46:30 +0900

    pointcut 학습

---
+++ src/me/gicheol/learningtest/jdk/pointcut/Bean.java
@@ -0,0 +1,11 @@
+package me.gicheol.learningtest.jdk.pointcut;
+
+import java.lang.reflect.Method;
+
+public class Bean {
+
+    public void method() throws RuntimeException {
+
+    }
+
+}
---
+++ src/me/gicheol/learningtest/jdk/pointcut/TargetInterface.java
@@ -0,0 +1,13 @@
+package me.gicheol.learningtest.jdk.pointcut;
+
+public interface TargetInterface {
+
+    void hello();
+
+    void hello(String a);
+
+    int minus(int a, int b) throws RuntimeException;
+
+    void method();
+
+}
