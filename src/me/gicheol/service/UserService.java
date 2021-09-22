f2d4577046529ec41d638b7174f08a2f538544bf
LeeGiCheol <asdqwek200@naver.com>
2021-09-22 21:18:40 +0900

    Transaction 설정 Annotation 변경

--- src/me/gicheol/service/UserService.java
+++ src/me/gicheol/service/UserService.java
@@ -1,21 +1,25 @@
 package me.gicheol.service;
 
 import me.gicheol.domain.User;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Transactional
 public interface UserService {
 
     void add(User user);
+
+    void update(User user);
 
     void upgradeLevels();
 
     void deleteAll();
 
+    @Transactional(readOnly = true)
     User get(String id);
 
+    @Transactional(readOnly = true)
     List<User> getAll();
-
-    void update(User user);
 
 }
