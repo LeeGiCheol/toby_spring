aa0a3e58b988d0279010a3fe16b278e336ca2d58
LeeGiCheol <asdqwek200@naver.com>
2021-09-26 20:04:53 +0900

    OXM 테스트

---
+++ src/me/gicheol/learningtest/jdk/oxm/OxmTest.java
@@ -0,0 +1,42 @@
+package me.gicheol.learningtest.jdk.oxm;
+
+import me.gicheol.sql.jaxb.SqlType;
+import me.gicheol.sql.jaxb.Sqlmap;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.oxm.Unmarshaller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import javax.xml.transform.stream.StreamSource;
+import java.io.IOException;
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+public class OxmTest {
+
+    @Autowired
+    Unmarshaller unmarshaller;
+
+    @Test
+    public void unmarshallSqlMap() throws IOException {
+        StreamSource xmlSource = new StreamSource(getClass().getResourceAsStream("sqlmap.xml"));
+        Sqlmap sqlmap = (Sqlmap) this.unmarshaller.unmarshal(xmlSource);
+
+        List<SqlType> sqlList = sqlmap.getSql();
+
+        assertThat(sqlList.size(), is(3));
+        assertThat(sqlList.get(0).getKey(), is("add"));
+        assertThat(sqlList.get(0).getValue(), is("insert"));
+        assertThat(sqlList.get(1).getKey(), is("get"));
+        assertThat(sqlList.get(1).getValue(), is("select"));
+        assertThat(sqlList.get(2).getKey(), is("delete"));
+        assertThat(sqlList.get(2).getValue(), is("delete"));
+    }
+
+}
