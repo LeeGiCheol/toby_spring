3d282962eb3d487de4691b4161d23db6f79a53e3
LeeGiCheol <asdqwek200@naver.com>
2021-10-04 22:01:16 +0900

    XML -> Java dataSource bean 제거

--- src/me/gicheol/test/TestApplicationContext.java
+++ src/me/gicheol/test/TestApplicationContext.java
@@ -1,9 +1,26 @@
 package me.gicheol.test;
 
+import com.mysql.jdbc.Driver;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ImportResource("classpath:/test-applicationContext.xml")
 public class TestApplicationContext {
+
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+        dataSource.setDriverClass(Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/test_toby_spring?serverTimezone=UTC");
+        dataSource.setUsername("toby_spring");
+        dataSource.setPassword("toby_spring");
+
+        return dataSource;
+    }
+
 }
