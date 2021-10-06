3a6c5c605491711b24f119e43d7f230b8b3bdd98
LeeGiCheol <asdqwek200@naver.com>
2021-10-06 22:01:59 +0900

    DataSource Properties @Value 사용 분리

--- src/me/gicheol/config/AppContext.java
+++ src/me/gicheol/config/AppContext.java
@@ -4,9 +4,9 @@ import com.mysql.jdbc.Driver;
 import me.gicheol.service.DummyMailSender;
 import me.gicheol.service.UserService;
 import me.gicheol.test.UserServiceTest.TestUserService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.*;
-import org.springframework.core.env.Environment;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.mail.MailSender;
@@ -23,25 +23,27 @@ import javax.sql.DataSource;
 @ComponentScan(basePackages = "me.gicheol")
 public class AppContext {
 
-    @Autowired
-    Environment env;
+    @Value("${db.driverClass}")
+    Class<? extends Driver> driverClass;
 
+    @Value("${db.url}")
+    String url;
+
+    @Value("${db.username}")
+    String username;
+
+    @Value("${db.password}")
+    String password;
     /**
      * DB연결과 트랜잭션
      */
     @Bean
     public DataSource dataSource() {
         SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
-
-        try {
-            dataSource.setDriverClass((Class<? extends java.sql.Driver>) Class.forName(env.getProperty("db.driverClass")));
-        } catch (ClassNotFoundException e) {
-            throw new RuntimeException(e);
-        }
-
-        dataSource.setUrl(env.getProperty("db.url"));
-        dataSource.setUsername(env.getProperty("db.username"));
-        dataSource.setPassword(env.getProperty("db.password"));
+        dataSource.setDriverClass(this.driverClass);
+        dataSource.setUrl(this.url);
+        dataSource.setUsername(this.username);
+        dataSource.setPassword(this.password);
 
         return dataSource;
     }
@@ -52,6 +54,15 @@ public class AppContext {
         transactionManager.setDataSource(dataSource());
 
         return transactionManager;
+    }
+
+    /**
+     * @Value를 사용하기 위해서 Bean 등록. static 으로 등록해야 한다.
+     * @return
+     */
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 
