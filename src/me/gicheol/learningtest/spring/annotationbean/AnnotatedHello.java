e6094695ed52321c95d14b97834ae25277f7750c
LeeGiCheol <asdqwek200@naver.com>
2021-10-11 17:29:56 +0900

    @Configuration 설정

--- src/me/gicheol/learningtest/spring/annotationbean/AnnotatedHello.java
+++ src/me/gicheol/learningtest/spring/annotationbean/AnnotatedHello.java
@@ -1,7 +1,4 @@
 package me.gicheol.learningtest.spring.annotationbean;
 
-import org.springframework.stereotype.Component;
-
-@Component("myAnnotatedHello")
 public class AnnotatedHello {
 }
--- src/me/gicheol/learningtest/spring/annotationbean/AnnotationBeanTest.java
+++ src/me/gicheol/learningtest/spring/annotationbean/AnnotationBeanTest.java
@@ -4,8 +4,7 @@ import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.assertThat;
 
 public class AnnotationBeanTest {
@@ -13,9 +12,12 @@ public class AnnotationBeanTest {
     @Test
     public void simpleBeanScanning() {
         ApplicationContext context = new AnnotationConfigApplicationContext("me.gicheol.learningtest.spring.annotationbean");
-        AnnotatedHello annotatedHello = context.getBean("myAnnotatedHello", AnnotatedHello.class);
+        AnnotatedHello annotatedHello = context.getBean("annotatedHello", AnnotatedHello.class);
+        AnnotatedHelloConfig annotatedHelloConfig = context.getBean("annotatedHelloConfig", AnnotatedHelloConfig.class);
 
         assertThat(annotatedHello, is(notNullValue()));
+        assertThat(annotatedHelloConfig, is(notNullValue()));
+        assertThat(annotatedHelloConfig.annotatedHello(), is(sameInstance(annotatedHello)));
     }
 
 }
